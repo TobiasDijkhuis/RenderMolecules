053723843134f263fd2b6287f681ca9c1a4855cb
TobiasDijkhuis <tobiasdijkhuis@hotmail.com>
2025-03-29 09:34:40 +0100

    Correctly index volumetric data. Created Bond class.

--- RenderMolecules.py
+++ RenderMolecules.py
@@ -2,9 +2,10 @@ import math
 import os
 import sys
 
-import matplotlib.pyplot as plt
 import numpy as np
 from pytessel import PyTessel
+
+from copy import deepcopy
 
 elementList = [
     "H",
@@ -31,14 +32,14 @@ elementList = [
 ]
 
 elementMass = [
-    1,
+    1.008,
     4,
     7,
     9,
     11,
-    12,
-    14,
-    16,
+    12.01,
+    14.007,
+    16.00,
     19,
     20,
     23,
@@ -79,10 +80,24 @@ vdwRadii = [
     2.11,
 ]
 
-bondLengths = {"HO": 2.0, "CO": 2.0, "CH": 2.0, "OO": 2.0, "HH": 0.75}
+# Bond lengths in Angstrom
+bondLengths = {
+    "HO": 2.0,
+    "CO": 2.0,
+    "CH": 2.0,
+    "OO": 2.0,
+    "HH": 0.75,
+    "NN": 1.5,
+    "HN": 1.0,
+    "CN": 1.5,
+    "CC": 1.5,
+}
 hydrogenBondLength = 3.5
 hydrogenBondAngle = 35
-sphereScale = 0.5
+sphereScale = 0.2
+
+BOHR_TO_ANGSTROM = 0.5291177249
+ANGSTROM_TO_BOHR = 1 / BOHR_TO_ANGSTROM
 
 
 class Atom:
@@ -94,6 +109,7 @@ class Atom:
             float(field) for field in self._splitString[1:]
         ]
         self._positionVector = np.array([self._x, self._y, self._z])
+        self.isAngstrom = False
 
         try:
             self._element = elementList[self._atomicNumber - 1]
@@ -134,6 +150,16 @@ class Atom:
     def getPositionVector(self) -> np.ndarray[float]:
         return self._positionVector
 
+    def positionBohrToAngstrom(self) -> None:
+        if self.isAngstrom:
+            raise ValueError()
+        self.isAngstrom = True
+        self.setPositionVector(self._positionVector*BOHR_TO_ANGSTROM)
+
+    def setPositionVector(self, newPosition) -> None:
+        self._positionVector = newPosition
+        self._x, self._y, self._z = newPosition
+
     def getElement(self) -> str:
         return self._element
 
@@ -149,6 +175,49 @@ class Atom:
     def __str__(self):
         return f"Atom with atomic number {self._atomicNumber} at position {self._positionVector}"
 
+class Bond:
+    def __init__(self, atom1Index, atom2Index, bondType, bondLength, interatomicVector, midpointPosition):
+        self._atom1Index, self._atom2Index = atom1Index, atom2Index
+        self._bondType = bondType
+        self._bondLength = bondLength
+        self._interatomicVector = interatomicVector
+        self._midpointPosition = midpointPosition
+
+    def getAtom1Index(self) -> int:
+        return self._atom1Index
+    
+    def getAtom2Index(self) -> int:
+        return self._atom2Index
+
+    def getBondType(self) -> str:
+        return self._bondType
+        
+    def getBondLength(self) -> float:
+        return self._bondLength
+
+    def getInteratomicVector(self)-> np.ndarray[float]:
+        return self._interatomicVector
+    
+    def getMidpointPosition(self) -> np.ndarray[float]:
+        return self._midpointPosition
+
+    def setMidpointPosition(self, midpointPosition: np.ndarray) -> None:
+        self._midpointPosition = midpointPosition
+
+    def getDirection(self) -> np.ndarray[float]:
+        return self._interatomicVector / self._bondLength
+
+    def getAxisAngleWithZaxis(self) -> tuple[float, float, float, float]:
+        z = np.array([0, 0, 1])
+        axis = np.cross(z,self._interatomicVector)
+        if np.linalg.norm(axis) < 1e-5:
+            axis = np.array([0,0,1])
+            angle = 0.0
+        else:
+            axis /= np.linalg.norm(axis)
+            angle = np.arccos(np.dot(self._interatomicVector,z)/self._bondLength)
+        return angle, axis[0], axis[1], axis[2]
+
 
 class CUBEfile:
     def __init__(self, filepath):
@@ -157,9 +226,17 @@ class CUBEfile:
             self._lines = file.readlines()
         self._nAtoms = int(self._lines[2].split()[0].strip())
 
+        if self._nAtoms < 0:
+            self._nAtoms = -self._nAtoms
         self._atoms = [0] * self._nAtoms
+        
         for i in range(self._nAtoms):
             self._atoms[i] = Atom(self._lines[6 + i].strip())
+
+            self._atoms[i].positionBohrToAngstrom()
+        
+        self._displacements = []
+        self._bonds = []
 
     def getAtoms(self):
         return self._atoms
@@ -170,27 +247,28 @@ class CUBEfile:
     def readVolumetricData(self) -> None:
         self._NX, self._NY, self._NZ = [
             int(self._lines[i].split()[0].strip()) for i in [3, 4, 5]
-        ]
+        ] 
         self._volumetricOriginVector = np.array(
             [float(i) for i in self._lines[2].split()[1:]]
-        )
+        ) * BOHR_TO_ANGSTROM
+
         self._volumetricAxisVectors = np.array(
             [[float(i) for i in self._lines[3 + i].split()[1:]] for i in [0, 1, 2]]
-        )
+        ) * BOHR_TO_ANGSTROM
 
-        self._volumetricData = np.zeros((self._NX, self._NY, self._NZ))
         volumetricLines = " ".join(
             line.strip() for line in self._lines[6 + self._nAtoms :]
         ).split()
-        dataIndexUpdate = 0
+
+        self._volumetricData = np.zeros((self._NX, self._NY, self._NZ))
         for ix in range(self._NX):
             for iy in range(self._NY):
                 for iz in range(self._NZ):
                     dataIndex = ix * self._NY * self._NZ + iy * self._NZ + iz
-                    # print(dataIndex)
-                    self._volumetricData[ix, iy, iz] = float(volumetricLines[dataIndex])
-                    dataIndexUpdate += 1
-
+                    self._volumetricData[ix, iy, iz] = float(
+                        volumetricLines[dataIndex]
+                    )
+                    
     def getFilepath(self) -> str:
         return self._filepath
 
@@ -215,12 +293,21 @@ class CUBEfile:
             raise ValueError(msg)
 
         pytessel = PyTessel()
+
+        unitCell = self._volumetricAxisVectors * self._volumetricData.shape
+
+        # Flatten the volumetric data such that X is the fastest moving index, according to the PyTessel documentation.
         vertices, normals, indices = pytessel.marching_cubes(
-            self._volumetricData.flatten(),
-            self._volumetricData.shape,
-            self._volumetricAxisVectors.flatten(),
+            self._volumetricData.flatten(order='F'),
+            reversed(self._volumetricData.shape),
+            unitCell.flatten(),
             isovalue,
         )
+
+        vertices += np.diag(0.5 * unitCell) + self._volumetricOriginVector
+        for displacement in self._displacements:
+            vertices += displacement
+
         pytessel.write_ply(filepath, vertices, normals, indices)
 
     def getTotalCharge(self) -> int:
@@ -235,7 +322,7 @@ class CUBEfile:
     def isRadical(self) -> bool:
         return self.getAmountOfElectrons() % 2 != 0
 
-    def getBondTuples(
+    def createBonds(
         self,
     ) -> list[tuple[int, int, str, float, np.ndarray[float], np.ndarray[float]]]:
         """For each bond, get the indices of atoms it connects, bond type, bond length, bond vector and bond midpoint vector"""
@@ -247,7 +334,8 @@ class CUBEfile:
             np.array([v[2] for v in allAtomPositions]),
         )
         x, y, z = allAtomPositionsTuples
-        bondTuples = []
+
+        connectingIndices = []
         for i, atom in enumerate(self._atoms):
             centralPos = allAtomPositions[i]
             centralElement = allAtomElements[i]
@@ -261,7 +349,7 @@ class CUBEfile:
             distSquared = dx * dx + dy * dy + dz * dz
             isBondedToCentral = np.nonzero(distSquared <= allowedBondLengthsSquared)[0]
             for atomIndex in isBondedToCentral:
-                connectingIndices = [(a[0], a[1]) for a in bondTuples]
+                #connectingIndices = [(a[0], a[1]) for a in bondTuples]
                 if atomIndex == i:
                     continue
                 if (i, atomIndex) not in connectingIndices and (
@@ -273,8 +361,7 @@ class CUBEfile:
                     ) / 2.0
                     bondLength = distSquared[atomIndex] ** 0.5
                     bondVector = allAtomPositions[i] - allAtomPositions[atomIndex]
-                    bondTuples.append(
-                        (
+                    newBond = Bond(
                             i,
                             atomIndex,
                             "".join(
@@ -283,49 +370,76 @@ class CUBEfile:
                             bondLength,
                             bondVector,
                             bondMidpoint,
-                        )
                     )
-        self._bondTuples = []
-        return bondTuples
+                    connectingIndices.append((i, atomIndex))
+                    self._bonds.append(newBond)
 
-    def generateBondOrderBond(self, bondTuple, bondOrder: int, angle=0.0):
-        """TODO: Add way to generate double bond if desired for a certain bond"""
-        index = self._bondTuples.index(bondTuple)
-        self._bondTuples.pop(index)
-        bondVector = bondTuple[-2]
-        raise NotImplementedError()
+    def generateBondOrderBond(
+        self,
+        bond: Bond,
+        bondOrder: int,
+        cameraPos: np.ndarray[float],
+        displacementScaler=0.2,
+    ) -> list[Bond]:
+        """Way to generate multiple bonds, for example in CO2 molecule double bonds, or CO triple bonds."""
+        if bondOrder == 1:
+            return
+        index = self._bonds.index(bond)
+        self._bonds.pop(index)
+        bondVector = bond.getInteratomicVector()
 
+        # Get a vector that is perpendicular to the plane given by the bondVector and vector between camera and bond midpoint.
+        displacementVector = np.cross(bondVector, bond.getMidpointPosition() - cameraPos)
+        displacementVector /= np.linalg.norm(displacementVector)
 
-def generatePerpendicularVector(vector: np.ndarray[float]) -> np.ndarray[float]:
-    # https://math.stackexchange.com/questions/137362/how-to-find-perpendicular-vector-to-another-vector
-    x, y, z = vector
-    perpVector = np.array(
-        [
-            math.copysign(vector[2], vector[0]),
-            math.copysign(vector[2], vector[1]),
-            -math.copysign(abs(vector[0]) + abs(vector[1]), vector[2]),
-        ]
-    )
-    perpVector /= np.linalg.norm(perpVector)
+        # If bondOrder is odd, then we also have displacementMag of 0.
+        if bondOrder % 2 == 0:
+            displacementMag = -displacementScaler / 4 * bondOrder
+        else:
+            displacementMag = -displacementScaler / 2 * (bondOrder - 1)
 
-    # Instead of doing this, we could also describe the plane generated by the two vectors
-    # in R3, the bondvector and the vector from the camera to the bond. Then, the displacement vector
-    # should be perpendicular to both these vectors, i.e. the third vector should be the cross product of
-    # the other two vectors. This third vector is then the displacement vector to generate a double bond.
-    # The camera location can be found as bpy.data.objects["Camera"].location,
-    # and then vector connecting camera and bond can be calculated as cam_log - bondMidpoint
-    return perpVector
+        # Create the bonds, and add them to self._bonds
+        for i in range(bondOrder):
+            bondAdjusted = deepcopy(bond)
+            bondAdjusted.setMidpointPosition(
+                bondAdjusted.getMidpointPosition() + displacementVector * displacementMag
+            )
+            self._bonds.append(bondAdjusted)
+            displacementMag += displacementScaler
+        return self._bonds
+
+    def getBonds(self) -> list[Bond]:
+        return self._bonds
+
+    def getCenterOfMass(self) -> np.ndarray[float]:
+        masses = np.array([atom.getMass() for atom in self._atoms])
+        atomPositions = self.getAtomPositionVectors()
+        COM = np.array(
+            sum(masses[i] * atomPositions[i] for i in range(self._nAtoms)) / sum(masses)
+        )
+        return COM
+
+    def setCOMto(self, newCOMposition):
+        COM = self.getCenterOfMass()
+        for atom in self._atoms:
+            atom.setPositionVector(atom.getPositionVector() - (COM-newCOMposition))
+        self._displacements.append(-(COM-newCOMposition))
+
+    def setAveragePositionToOrigin(self):
+        averagePosition = np.average(np.array([atom.getPositionVector() for atom in self._atoms]), axis=0)
+        for atom in self._atoms:
+            atom.setPositionVector(atom.getPositionVector() - averagePosition)
+        self._displacements.append(-averagePosition)
 
 
 if __name__ == "__main__":
-    CUBE = CUBEfile("H2O_elf.cube")
-    atomPositions = CUBE.getAtomPositionVectors()
-    atoms = CUBE.getAtoms()
-    masses = [atom.getMass() for atom in atoms]
-
-    bondTuples = CUBE.getBondTuples()
-    # CUBE.generateDoubleBond(bondTuples[0])
-    print(bondTuples)
-    print(generatePerpendicularVector(bondTuples[0][-2]))
-    # CUBE.readVolumetricData()
-    # CUBE.writePLY("H2O_elf.ply", 0.5)
+    CUBEfilepath = "H2C3N_B3LYP-D4_spindensity.cube"
+    #CUBEfilepath = "H2O_elf.cube"
+    #CUBEfilepath = "hexazine.cube"
+    CUBEfilepath_noext = os.path.splitext(CUBEfilepath)[0]
+    
+    CUBE = CUBEfile(CUBEfilepath)
+    CUBE.setCOMto(np.array([0, 0, 0]))
+    CUBE.readVolumetricData()
+    value = 0.02
+    #CUBE.writePLY(f"{CUBEfilepath_noext}_{value}.ply", value)
