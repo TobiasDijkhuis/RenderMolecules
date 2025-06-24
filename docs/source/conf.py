afda6f75ddf52e45479d83bb60493a6e49227637
Tobias Dijkhuis <tobiasdijkhuis@hotmail.com>
2025-06-24 17:02:46 +0200

    Fixed sphinx bux

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,11 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 from __future__ import annotations
 
-import render_molecules
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.append(os.path.abspath(os.path.join(__file__, "../../..")))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
